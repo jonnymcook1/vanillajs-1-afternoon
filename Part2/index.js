a88ea3759e695ac6a8985a270820759df9dc5c59
johncook <jonnymcook1@gmail.com>
2019-05-06 19:32:39 -0500

    final commit

---
+++ Part2/index.js
@@ -0,0 +1,15 @@
+// function test() {
+//     console.log("The house always win")
+// }
+
+const idInput = document.getElementById("idInput")
+const colorInput = document.getElementById("colorInput")
+
+function setCard() {
+    const card = document.getElementById(idInput.value)
+    card.style.color = colorInput.value
+
+}
+
+// console.log(idInput)
+// console.log(colorInput)
